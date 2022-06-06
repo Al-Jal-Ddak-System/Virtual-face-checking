7cb1055eb9dbddd569ccd6ed1ec1a9859a30cce3
baegyu jung <baelanche@naver.com>
2022-06-06 09:33:36 -0900

    edit server

--- lcd/lcd.py
+++ lcd/lcd.py
@@ -14,22 +14,25 @@ class socketInfo(SocketInfo):
 def run():
     display = drivers.Lcd()
     display.lcd_backlight(1)
-    csock = socket(AF_INET, SOCK_STREAM)
-    csock.connect(socketInfo.ADDR)
+    display.lcd_display_string("Greetings Human!", 1)  
+    display.lcd_display_string("Demo Pi Guy code", 2)  
+    sleep(2)  
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(socketInfo.ADDR)
 
     while(True):
-        command = csock.recv(socketInfo.BUFSIZE)
+        command = s.recv(socketInfo.BUFSIZE)
         command = str(command)
+        print(command)
         command = command.replace('b\'', '')
         command = command.replace('\\x00N=\\xf6', '')
         command = command.replace('\'', '')
-        display.lcd_display_string("SERVER MESSAGE", 1)  # Write line of text to first line of display
+        display.lcd_display_string("EMOTION DETECTED!", 1)  # Write line of text to first line of display
         display.lcd_display_string(command, 2)  # Write line of text to second line of display
-        print(command)
         
         sleep(1)
         display.lcd_clear()
-    csock.close()
+    s.close()
 
 
 
